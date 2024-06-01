448b0eb5bc37ed5416b1b62777c809ccbeaf4bd6
momenw2 <momen2550@icloud.com>
2024-06-02 01:43:45 +0700

    Create countryUniversity Model

---
+++ Models/countryUniversities.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const countryUniversitiesSchema = new Schema({
+  country: String,
+  university: [
+    {
+      name: String,
+      img: String,
+      QSRank: String,
+      countryUniversityURL: String,
+    },
+  ],
+});
+
+const countryUniversity = mongoose.model(
+  "countryUniversity",
+  countryUniversitiesSchema
+);
+
+module.exports = countryUniversity;
